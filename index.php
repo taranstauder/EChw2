2225ea23d4c19c5d4e7ebd80b45649e9391717e4
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-08-31 14:37:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
-	<h2>Welcome to my website!</h2>
-	<p>Some content goes here! Let's go with the classic "lorem ipsum."</p>
+	<h2>This is my Homework 2 site!</h2>
+	<p>There is some filler text below. I might just put some song lyrics.</p>
 
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
