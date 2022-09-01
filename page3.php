b290b4ba913a673f890ec7533236c4be88bc09fd
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-01 18:42:34 -0500

    Create page3.php

---
+++ page3.php
@@ -0,0 +1,27 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
+
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+	<h2>This is my Homework 2 site!</h2>
+	<p>There is some filler text below. I might just put some song lyrics.</p>
+
+	<p>
+		texas sucks
+	</p>
+	<p>
+		booo lincoln
+	</p>
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
