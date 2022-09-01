681f837f04b55183d28d6fea0288188001b73bc1
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-01 18:28:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,10 +14,21 @@
 	<p>There is some filler text below. I might just put some song lyrics.</p>
 
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		Boomer Sooner, Boomer Sooner 
+Boomer Sooner, Boomer Sooner 
+Boomer Sooner, Boomer Sooner 
+Boomer Sooner, OK U! 
+ 
+Oklahoma, Oklahoma 
+Oklahoma, Oklahoma 
+Oklahoma, Oklahoma 
+Oklahoma, OK U! 
 	</p>
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		I'm a Sooner born and Sooner bred 
+and when I die, I'll be Sooner dead 
+Rah Oklahoma, Rah Oklahoma 
+Rah Oklahoma, OK U! 
 	</p>
 </div>
 
