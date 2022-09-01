7df2a513fe779d31f2bdf71829b397cf84a1578b
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-01 18:41:40 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,1 +1,27 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
 
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+	<h2>This is my Homework 2 site!</h2>
+	<p>There is some filler text below. I might just put some song lyrics.</p>
+
+	<p>
+		boom soon
+	</p>
+	<p>
+		baker mayfield
+	</p>
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
