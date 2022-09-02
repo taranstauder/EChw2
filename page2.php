36f70fba2fdf1d4f947c9ee3c356afb27f348058
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-01 19:27:26 -0500

    Update page2.php

--- page2.php
+++ page2.php
@@ -10,15 +10,8 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
-	<h2>This is my Homework 2 site!</h2>
-	<p>There is some filler text below. I might just put some song lyrics.</p>
-
-	<p>
-		boom soon
-	</p>
-	<p>
-		baker mayfield
-	</p>
+	<h2>This is the second page of my Homework 2 site!</h2>
+	<h2>Notice the php sections</h2>
 </div>
 
 <?php include("includes/footer.php");?>
