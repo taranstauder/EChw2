ddd4f3d313e10a6b2a2718f385f5008e521017f0
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-07 14:05:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,15 +10,15 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
+	    
 	<h2>This is my Homework 2 site!</h2>
-	<p>There is some filler text below. I might just put some song lyrics. Actually, that might flag on turn it in, so I'll put nonsense.</p>
+	<p>Please fill out the below form.</p>
+<form action="handlepost.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
 
-	<p>
-		Tulsa, Houston, Dogs, Water, Air, Plane
-	</p>
-	<p>
-		Word, Number, Apple, Microsoft, Bean bag
-	</p>
 </div>
 
 <?php include("includes/footer.php");?>
