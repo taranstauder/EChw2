b44bef125c6e62564a19f68046c3f12c39528baf
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-07 14:22:50 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -10,8 +10,7 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
-	<h2>This is the second page of my Homework 2 site!</h2>
-	<h3>Post response</h3>
+	<h1>Post response</h1>
     Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
