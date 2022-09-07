0f895864984dbc80d12f93c021ab05652a603085
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-07 13:43:45 -0500

    Update navigation.php

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,5 +1,5 @@
 <div class="container">
-	<ul class="nav nav-pills">
+	<ul class="nav nav-tabs">
 	  <li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Index") {?>active<?php }?>" href="index.php">Home</a>
 	  </li>
