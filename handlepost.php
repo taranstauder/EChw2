6757c3256378b76287b68753a55450e16aef0596
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-07 14:07:56 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -1,7 +1,25 @@
-  <body>
-    <h1>Post response</h1>
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
+
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+	<h2>This is the second page of my Homework 2 site!</h2>
+	<h1>Post response</h1>
     Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
     <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
+  
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
