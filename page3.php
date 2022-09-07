86540d05297bbdddac1a9c4ea82f1ae9c3e9da46
taranstauder <98139764+taranstauder@users.noreply.github.com>
2022-09-07 14:23:05 -0500

    Update page3.php

--- page3.php
+++ page3.php
@@ -10,7 +10,7 @@
 <?php include("includes/navigation.php");?>
 
 <div class="container" id="main-content">
-	<h2>Get response</h2>
+	<h1>Get response</h1>
     Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
